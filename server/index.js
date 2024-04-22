e372273c1a5bfa0c18f6abdddfff39a88135b88c
vijaykanwal90 <vijaykanwal90@gmail.com>
2024-04-23 01:15:25 +0530

    cors method changed

--- server/index.js
+++ server/index.js
@@ -37,7 +37,8 @@ app.use(bodyParser.json({limit:"30mb",extended:true}));
 app.use(bodyParser.urlencoded({limit:"30mb",extended:true}));
 app.use(cors({
     origin:'https://social-media-app-frontend-weld.vercel.app/',
-    credentials:true
+    credentials:true,
+    methods:['POST','GET','PATCH']
 }));
 app.use("/assets",express.static(path.join(__dirname,'public/assets')));
 
