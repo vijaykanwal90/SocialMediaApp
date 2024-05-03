f9ba635257a11ce87ee89158f70c609aa6180bf7
vijaykanwal90 <vijaykanwal90@gmail.com>
2024-05-04 01:39:53 +0530

    monggose connection done

--- server/database/index.js
+++ server/database/index.js
@@ -1,12 +1,15 @@
 import mongoose from "mongoose";
-// import {DB_NAME} from "../constants.js";
-
+import dotenv from "dotenv"
+dotenv.config({
+    path: './.env'
+})
 
 const connectDB = async ()=>{
     try {
-        // console.log(process.env.MONGODB_URL)
-       const connectionIstance =  await mongoose.connect(`${process.env.MONGODB_URL}/${process.env.DB_NAME}`);
-       console.log(`\n MongoDB connected !!  DB HOST ${connectionIstance.connection.host}`);
+        console.log(process.env.PORT)
+      console.log("on db connection")
+       const connectionInstance =  await mongoose.connect(`${process.env.MONGODB_URL}/${process.env.DB_NAME}`);
+       console.log(`\n MongoDB connected !!  DB HOST ${connectionInstance.connection.host}`);
     } catch (error) {
         console.log("MONGODB connection error ",error)
         process.exit(1);
--- server/index.js
+++ server/index.js
@@ -1,78 +1,17 @@
-// *** dependency imports ***
-import express from "express";
-import bodyParser from "body-parser";
-import mongoose from "mongoose";
-import cors from "cors";
+import connectDB from "./database/index.js"
 import dotenv from "dotenv"
-import multer from "multer";
-import helmet from "helmet";
-import morgan from "morgan";
-import path from "path";
-import { fileURLToPath } from "url";
-// *** route imports ***
+import mongoose from "mongoose";
+import {app}  from "./app.js"
+dotenv.config({
+    path: './.env'
+})
 
-import authRoutes from "./routes/auth.route.js"
-import userRoutes from "./routes/user.route.js"
-import postRoutes from "./routes/post.route.js"
-// *** models imports***
-import User from "./models/user.model.js"
-import Post from "./models/post.model.js";
-import {users, posts} from "./data/index.js"
-// *** controller imports ***
-
-import {register} from "./controllers/auth.controller.js"
-import {createPost} from "./controllers/post.controller.js"
-// ***middleware import ***
-import { verifyToken } from "./middleware/auth.middleware.js";
-// configureation of midllewares
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
-dotenv.config();
-const app = express();
-app.use(express.json());
-app.use(helmet());
-app.use(helmet.crossOriginResourcePolicy({policy:"cross-origin"}));
-app.use(morgan("command"));
-app.use(bodyParser.json({limit:"30mb",extended:true}));
-app.use(bodyParser.urlencoded({limit:"30mb",extended:true}));
-app.use(cors({
-    origin:'https://social-media-app-frontend-weld.vercel.app',
-    credentials:true
-    // methods:['POST','GET','PATCH']
-}));
-app.use("/assets",express.static(path.join(__dirname,'public/assets')));
-
-
-// file storage
-
-const storage = multer.diskStorage({
-    destination:function(req,file,cb){
-        cb(null,"public/assets");
-    },
-    filename:function(req,file,cb){
-        cb(null,file.originalname);
-    }
-});
-
-
-const upload = multer({storage})
-
-
-// routes with files
-app.post("/auth/register",upload.single("picture"),register)
-app.post("/posts",verifyToken,upload.single("picture"),createPost);
-
-// routes
-app.use("/api/v1/auth",authRoutes)
-app.use("/api/v1/users",userRoutes)
-app.use("/posts",postRoutes)
-// set up of mongoses
-
+connectDB()
+console.log(process.env.MONGO_URL)
 const PORT = process.env.PORT || 5434;
-console.log(PORT)
-mongoose.connect(process.env.MONGO_URL,{
-    useNewUrlParser:true,
-    useUnifiedTopology:true,
+
+mongoose.connect(process.env.MONGODB_URL,{
+ 
 }).then(()=>{
     app.listen(PORT ,()=>{
         console.log(PORT)
@@ -80,5 +19,5 @@ mongoose.connect(process.env.MONGO_URL,{
     })
 })
 .catch((error)=>{
-    console.log("MongoDB connection failed ||", err);
+    console.log("MongoDB connection failed ||", error);
 })
