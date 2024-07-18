8f45e8af18e797fcea310cfe0bc1b56da8ffd9e4
vijaykanwal90 <vijaykanwal90@gmail.com>
2024-07-18 16:31:37 +0530

    home route

--- server/index.js
+++ server/index.js
@@ -51,6 +51,11 @@ const storage = multer.diskStorage({
 const upload = multer({ storage });
 
 /* ROUTES WITH FILES */
+app.get("/",(req,res)=>{
+    res.send("Hlow world")
+})
+
+
 app.post("/register", upload.single("picture"), register);
 app.post("/posts", verifyToken, upload.single("picture"), createPost);
 
